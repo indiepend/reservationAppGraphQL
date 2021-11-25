8f88d6bd622fb7de4836b0f290c5f3025b78784b
Robert <robert.p.dworak@gmail.com>
2021-11-25 18:47:11 +0100

    add files

---
+++ src/main/java/com/restaurant/reservationAppGraphQL/ReservationAppGraphQlApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant.reservationAppGraphQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationAppGraphQlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationAppGraphQlApplication.class, args);
+	}
+
+}
